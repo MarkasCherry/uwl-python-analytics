98ac8d62767f3166e74aef143f480c45e3d4c768
MarkasCherry <markas.cherry@gmail.com>
2021-05-01 16:32:12 +0200

    Completed exercise 2 of seminar 8

--- seminar8.py
+++ seminar8.py
@@ -7,6 +7,8 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 
+# Exercise 1 tasks and outputs:
+
 z = np.arange(1, 21, 1).reshape(4, 5)
 
 ls = np.linspace(1, 5, 10)
@@ -17,7 +19,6 @@ y = []
 for i in x:
     y.append(math.sin(i))
 
-
 print('Shape of z: ', z.shape)
 print('Length of z: ', z.size)
 print('10 numbers between 1 - 5: ', ls)
@@ -27,3 +28,14 @@ print('Array y: ', y)
 plt.plot(x, y)
 plt.grid()
 plt.show()
+
+# Exercise 2 tasks and outputs:
+
+print('3rd row of z: ', z[2])
+print('4th column of z: ', [dim[4] for dim in z])
+print('Mask true, where z[i] > 10: ', z > 10)
+
+z = np.where(z > 10, 5, z)
+print('New z = ', z)
+print('Sum of each z column: ', np.sum(z, axis=0))
+print('Sum of each z row: ', np.sum(z, axis=1))
